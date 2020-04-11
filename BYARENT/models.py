fa06f2b251ca1324b64bb55749039bb3179ea5c8
omidiora <omidioraemmanuel@gmail.com>
2020-04-11 20:19:01 +0100

    Bryrent

---
+++ BYARENT/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Home(models.Model):
+    name=model.CharField(max_length=200)
+    description=model.TextField(max_length=200)
+    name=model.CharField(max_length=200)
+    name=model.CharField(max_length=200)
